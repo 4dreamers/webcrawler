eac36146a85ff1f8986a10303987a0a8ca59ab76
4dreamers <56182026+4dreamers@users.noreply.github.com>
2019-11-07 19:46:09 -0500

    update constants

--- src/main/java/com/scalable/webcrawler/Constants.java
+++ src/main/java/com/scalable/webcrawler/Constants.java
@@ -2,8 +2,8 @@ package com.scalable.webcrawler;
 
 public class Constants {
 	
-	public final static String GOOGLE_CX = "001936359397208676040:v1u44zeqomu"; // My custom search engine
-	public final static String GOOGLE_API_KEY = "AIzaSyAP6yNYlaeLtw7VBOgV8lGW0xmN359JeW8"; // Search engine api_key
+	public final static String GOOGLE_CX = "001936359397208676040"; // My custom search engine
+	public final static String GOOGLE_API_KEY = "AIzaSyAP6yNYlaeLxmN359JeW8"; // Search engine api_key
 	public final static long   SEARCH_RESULT_NUMBER = 5;
 	
 }
